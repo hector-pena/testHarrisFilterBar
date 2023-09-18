6f722851c75e35e88514a764176f92db56779208
Hector Pena <hpenamendoza@its.com>
2023-09-17 20:55:17 -0400

    Create filter bar

---
+++ src/App.js
@@ -0,0 +1,186 @@
+// import logo from './logo.svg';
+import './App.css';
+import AppBarMenuFilter from './components/AppBarMenuFilter';
+// 'use strict';
+
+import React, {
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+
+import { AgGridReact } from '@ag-grid-community/react';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+function App() {
+
+  const gridRef = useRef();
+  const [scrollMode, setScrollMode] = useState('vertical');
+
+  // date formatting
+  const dateFormatter = (params) => {
+    let date = new Date(params.value);
+    let day = date.getDate().toString().padStart(2, '0');
+    let month = (date.getMonth() + 1).toString().padStart(2, '0');
+    let year = date.getFullYear().toString();
+    let hourNum = date.getHours() % 12;
+    let hour = (hourNum === 0 ? 12 : hourNum).toString().padStart(2, '0');
+    let min = date.getMinutes().toString().padStart(2, '0');
+    let amPM = date.getHours() < 12 ? 'AM' : 'PM';
+
+    return ( day +'/' +month +'/' +year +' ' +hour +':' +min +' ' +amPM);
+  };
+
+  const [rowData, setRowData] = useState(
+    [
+      { "patientName": "Francis, Henry", "resource": "Mc Coy Henry", "apptType": "Sick Visit", "apptTime": "2020-05-30T10:01:00" , "status": "Ready for Provider 9:26 AM", "wait": "00:05" },
+      { "patientName": "Murphy, Catrine", "resource": "Mc Coy Henry", "apptType": "Sick Visit", "apptTime":"2015-04-21T16:30:00" , "status": "", "wait": "" },
+      { "patientName": "Murphy, Alicia", "resource": "Anna Bates", "apptType": "Sick Visit", "apptTime":"2010-02-19T12:02:00" , "status": "Checked in 9:57 AM", "wait": "00:05" },
+      { "patientName": "Murphy, Alicia", "resource": "Mc Coy Henry", "apptType": "Sick Visit", "apptTime":"1995-10-04T03:27:00" , "status": "", "wait": "" },
+      { "patientName": "Francis, Henry", "resource": "Mc Coy Henry", "apptType": "Sick Visit", "apptTime":"1995-10-04T03:27:00" , "status": "In Re-schedule Queue", "wait": "" },
+      { "patientName": "Pandy, Peggy", "resource": "Mc Coy Henry", "apptType": "CQM New", "apptTime":"1995-10-04T03:27:00" , "status": "", "wait": "" },
+      { "patientName": "Murphy, Alicia", "resource": "Anna Bates", "apptType": "Sick Visit", "apptTime":"1995-10-04T03:27:00" , "status": "Checked in 9:57 AM", "wait": "00:05" },
+      { "patientName": "Murphy, Alicia", "resource": "Mc Coy Henry", "apptType": "Sick Visit", "apptTime":"1995-10-04T03:27:00" , "status": "", "wait": "" },
+      { "patientName": "Murphy, Alicia", "resource": "Anna Bates", "apptType": "Sick Visit", "apptTime":"1995-10-04T03:27:00" , "status": "Checked in 9:57 AM", "wait": "00:05" },
+      { "patientName": "Murphy, Alicia", "resource": "Mc Coy Henry", "apptType": "Sick Visit", "apptTime":"1995-10-04T03:27:00" , "status": "", "wait": "" }
+    ]
+  );
+
+  const [columnDefs, setColumnDefs] = useState(
+    [
+      { headerName: 'PATIENT NAME', field: 'patientName', sortable: true, headerCheckboxSelection: true, checkboxSelection: true, showDisabledCheckboxes: true, minWidth: 250},
+      { headerName: 'RESOURCE', field: 'resource', sortable: true, minWidth: 250 },
+      { headerName: 'APPT TYPE', field: 'apptType', sortable: true, minWidth: 150 },
+      { headerName: 'APPT TIME', field: 'apptTime', sortable: true, minWidth: 150, valueFormatter: dateFormatter},
+      { headerName: 'STATUS', field: 'status', sortable: true, minWidth: 300 },
+      { headerName: 'WAIT', field: 'wait', sortable: true, minWidth: 150 }
+    ]
+  );
+
+  const [filterOptionsList, setFilterOptionsList] = useState([
+    { headerName: 'Dates' },
+    { headerName: 'Status' },
+    { headerName: 'People' },
+    { headerName: 'Locations' },
+    { headerName: 'Rooms' },
+    { headerName: 'Views' }
+  ]);
+
+  const [rowDataStr, setRowDataStr] = useState(JSON.stringify(rowData, undefined, 2));
+  const [columnDefsStr, setColumnDefStr] = useState(JSON.stringify(columnDefs, undefined, 2));
+  const [filterOptionsListStr, setFilterOptionsListStr] = useState(JSON.stringify(filterOptionsList, undefined, 2));
+
+  const editRowData = e => {
+    let rowDataText =  e.target.value;
+    setRowDataStr(rowDataText);
+    setRowData(JSON.parse(rowDataText))
+  };
+
+  const editColumnDefs = e => {
+    let columnDefsText = e.target.value;
+    setColumnDefStr(columnDefsText);
+    setColumnDefs(JSON.parse(columnDefsText));
+  }
+
+  const editFilterData = e => {
+    let filterDataText =  e.target.value;
+    setFilterOptionsListStr(filterDataText);
+    setFilterOptionsList(JSON.parse(filterDataText))
+  };
+
+  const changeScrollMode = e => {
+    let scrollModeSelected = e.target.value;
+    setScrollMode(scrollModeSelected);
+  };
+
+
+  // default column configuration
+  const defaultColDef = useMemo(() => {
+    return {
+      resizable: true,
+    };
+  }, []);
+
+  const onFirstDataRendered = useCallback((params) => {
+    gridRef.current.api.sizeColumnsToFit();
+  }, []);
+
+   // run when Grid Size event changed
+   const onGridSizeChanged = useCallback((params) => {
+    // get the current grids width
+    let gridWidth = document.getElementById('grid-wrapper').offsetWidth;
+    // get current select scroll mode value
+    let mode = document.getElementById('select-mode').value;
+    // keep track of which columns to hide/show
+    let columnsToShow = [];
+    let columnsToHide = [];
+    // iterate over all columns (visible or not) and work out
+    // now many columns can fit (based on their minWidth)
+    let totalColsWidth = 0;
+    let allColumns = gridRef.current.columnApi.getColumns();
+    if (allColumns && allColumns.length > 0) {
+      for (let i = 0; i < allColumns.length; i++) {
+        let column = allColumns[i];
+        totalColsWidth += column.getMinWidth() || 0;
+        if (totalColsWidth > gridWidth) {
+          columnsToHide.push(column.getColId());
+        } else {
+          columnsToShow.push(column.getColId());
+        }
+      }
+    }
+    // show/hide columns based on current grid width
+    if(mode === 'both') {
+      gridRef.current.columnApi.setColumnsVisible(columnsToHide, true);
+    } else {
+      gridRef.current.columnApi.setColumnsVisible(columnsToShow, true);
+      gridRef.current.columnApi.setColumnsVisible(columnsToHide, false);
+    }
+
+    // fill out any available space to ensure there are no gaps
+    gridRef.current.api.sizeColumnsToFit();
+    
+  }, []);
+
+  return (
+
+    <div>
+      <h2>Harris React Grid</h2>
+      <br/>
+      <div className='textBox'>
+        <textarea value={rowDataStr} onChange={editRowData} rows="30" cols="50"></textarea>
+        <textarea value={columnDefsStr} onChange={editColumnDefs} rows="30" cols="50"></textarea>
+        <textarea value={filterOptionsListStr} onChange={editFilterData} rows="30" cols="50"></textarea>
+      </div>
+      <AppBarMenuFilter filterOptionsList={filterOptionsList} />
+      <div id="grid-wrapper" className={scrollMode}  >
+      <AgGridReact
+        className='ag-theme-alpine'
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
+        rowSelection='multiple'
+        onFirstDataRendered={onFirstDataRendered}
+        onGridSizeChanged={onGridSizeChanged}
+      />
+      </div>
+      <div className='selectScroll'>
+        <p>Scroll Mode: 
+          <select id='select-mode' value={scrollMode} onChange={changeScrollMode}>
+            <option value="vertical">Vertical</option>
+            <option value="both" >Both</option>
+            <option value="none" >None</option>
+          </select>
+        {scrollMode}
+      </p>
+      </div>     
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/components/AppBarMenuFilter.jsx
@@ -0,0 +1,226 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreHoriz';
+import ExportIcon from '@mui/icons-material/VerticalAlignBottom';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import FilterOption from './FilterOption';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '#052E40'
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar({ filterOptionsList }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{ background: '#FDFDFD', color: '#2C2C2C', borderRadius: '10px' }}>
+        <Toolbar>
+          <Search style={{ border: '1px solid #E9E9E9', borderRadius: '6px' }}>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            {filterOptionsList.map((filter) => {
+                return <FilterOption filterName={filter} />
+            })}
+          </Box>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              color="inherit"
+              style={{ zIndex: 10 }}
+            >
+              <FilterListIcon />
+            </IconButton>
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }} style={{ padding: '10px 10px 10px 0', marginLeft: '-15px' }}>
+                <FilterOption filterName={{ headerName: 'All filters' }} filterOptionsList={filterOptionsList} />
+            </Box>
+          </Box>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <div style={{ width: '1px', height: '36px', background: '#E9E9E9', margin: '0 10px 0 0' }}></div>
+          </Box>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <ExportIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
